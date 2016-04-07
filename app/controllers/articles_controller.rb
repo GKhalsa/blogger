68976206d58501eda4ff7543152e1662f97e7ef9
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-04-06 22:48:10 -0600

    iter 1 complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,41 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    flash.notice = "Article was created!"
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "You don killed off ol '#{@article.title}'"
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title} Updated'"
+
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
+
+
 end
